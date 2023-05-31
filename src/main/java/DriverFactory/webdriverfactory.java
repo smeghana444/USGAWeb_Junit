1dabd8c043ee9a8845bb82a432ef840204949d20
Meghana, Soma <someghana@192.168.1.3>
2023-05-31 12:28:04 +0530

    commit changes

--- src/main/java/DriverFactory/webdriverfactory.java
+++ src/main/java/DriverFactory/webdriverfactory.java
@@ -1,9 +1,12 @@
 package DriverFactory;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
@@ -28,15 +31,12 @@ public class webdriverfactory
 			
 			
 		}
-		else if(browser.equalsIgnoreCase("firefox"))
-		{
-			WebDriverManager.firefoxdriver().setup();
-			tlDriver.set(new FirefoxDriver());
-		}
 		else if(browser.equals("edge"))
 		{
 			WebDriverManager.edgedriver().setup();
-			tlDriver.set(new EdgeDriver());
+			EdgeOptions options = new EdgeOptions();
+			options.addArguments("--remote-allow-origins=*");
+			tlDriver.set(new EdgeDriver(options));
 			
 		}
 		getDriver().manage().deleteAllCookies();
