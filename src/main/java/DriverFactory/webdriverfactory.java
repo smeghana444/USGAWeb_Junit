7decb12515aa10ba81f19977bd130d155c8e1966
Meghana, Soma <someghana@192.168.1.4>
2023-06-07 11:17:54 +0530

    commit changes

--- src/main/java/DriverFactory/webdriverfactory.java
+++ src/main/java/DriverFactory/webdriverfactory.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import util.ScreenRecorderUtil;
+
 
 public class webdriverfactory
 
